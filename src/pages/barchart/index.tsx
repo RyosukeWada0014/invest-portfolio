bb30f1400d30ed38d6dcb03d3e10f454a36c3ac3
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-14 22:28:13 +0900

    ✨ 棒グラフページの追加

---
+++ src/pages/barchart/index.tsx
@@ -0,0 +1,15 @@
+﻿import { NextPage } from "next";
+import Link from "next/link";
+
+const BarChart: NextPage = () => {
+  return (
+    <div>
+      <div>Bar Chart</div>
+      <Link href="/">
+        <a>戻る</a>
+      </Link>
+    </div>
+  );
+};
+
+export default BarChart;
