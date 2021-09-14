c174b85bb27e9eba9c0c4e809263e32e1bbf1666
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-14 22:25:12 +0900

    ✨ 円グラフページの追加

---
+++ src/pages/piechart/index.tsx
@@ -0,0 +1,15 @@
+﻿import { NextPage } from "next";
+import Link from "next/link";
+
+const PieChart: NextPage = () => {
+  return (
+    <div>
+      <div>pie Chart</div>
+      <Link href="/">
+        <a>戻る</a>
+      </Link>
+    </div>
+  );
+};
+
+export default PieChart;
