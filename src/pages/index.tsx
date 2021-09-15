12e3da1e5cff392ed1df221cfb9eec8ed99eec0f
RyosukeWada0014 <fbdp.14@gmail.com>
2021-09-15 21:44:15 +0900

    ✨ ヘッダーの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,20 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import Link from "next/link";
-import styles from "src/styles/Home.module.css";
+import { Header } from "src/component/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Link href="/piechart">
-        <a>pieChart</a>
-      </Link>
-      <Link href="/barchart">
-        <a>barChart</a>
-      </Link>
-      <footer className={styles.footer}>
+    <div>
+      <Header />
+      <div className="text-3xl h-96 text-center">資産管理や</div>
+      <footer className="text-center">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{" "}
-          <span className={styles.logo}>
+          <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
