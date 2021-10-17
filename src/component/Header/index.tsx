1803737610d6c74b0ae95be805f901c1ca1fbf31
RyosukeWada0014 <fbdp.14@gmail.com>
2021-10-17 18:39:30 +0900

    🐛 mapのkeyの修正

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -13,9 +13,9 @@ const NAV_ITEMS = [
 export const Header: VFC = () => {
   return (
     <header className="flex justify-center items-center h-24 w-full">
-      {NAV_ITEMS.map((item) => {
+      {NAV_ITEMS.map((item, index) => {
         return (
-          <Link href={item.href} key={item.href}>
+          <Link href={item.href} key={index.toString()}>
             <a className="inline-block text-xl p-3 mx-2 hover:text-blue-600 active:text-blue-600">
               {item.label}
             </a>
